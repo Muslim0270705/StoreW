340cc0470539b610e8c0442005dde98f0d139418
Muslim <magezamuslim40@gmail.com>
2023-09-07 20:12:46 +0600

    finish

---
+++ src/components/Layout/Header/Header.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import logo from "../../../photos/usave.png"
+import {Link} from "react-router-dom";
+import {CgProfile} from "react-icons/cg"
+const Header = () => {
+    return (
+        <section id="header">
+            <a href="#"><img src={logo} className="logo" alt="logoo"/></a>
+
+            <div>
+                <ul id="navbar">
+                    <li><a className="active" href="index.html">Home</a></li>
+                    <li><Link to={"/shop"}>Shop</Link></li>
+                    <li><Link to={'/about'}>About</Link></li>
+                    <li><Link to={"/contact"}>Contact</Link></li>
+                    <li id="lg-bag"><Link to={"/cart"}><i className="fa fa-shopping-bag"></i></Link></li>
+                    <li id="lg-bag"><Link to={"/register"}><CgProfile/></Link></li>
+                    <a href="#" id="close" className="fa fa-times"></a>
+
+                </ul>
+            </div>
+
+            <div id="mobile">
+
+                <a href="cart.html"><i className="fa fa-shopping-bag"></i></a>
+                <i id="bar" class="fas fa-outdent"></i>
+            </div>
+
+        </section>
+    );
+};
+
+export default Header;
